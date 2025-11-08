5a219216a62ead144dd74d31965b405081a7991f
FaatehNaeem <faatehnaeem53@gmail.com>
2025-11-08 22:36:58 +0500

    cleanup code and added .env to .gitignore

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 
-import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
 import {
@@ -51,42 +50,6 @@ import LoadingBar from 'react-top-loading-bar'
 
 export default App
 
-// export default class App extends Component {
-//  pageSize = 12;
-//  apiKey = process.env.REACT_APP_NEWS_API
-//  state={
-//   progress:0
-//  }
-//  setProgress=(progress)=>{
-//   setState({progress: progress})
-//  }
-//   render() {
-//     return (
-//       <div>
-          
-
-//         <BrowserRouter>
-//           <Navbar />
-//           <LoadingBar
-//         color='#f11946'
-//         progress={state.progress}
-//         height={3}
-//         // onLoaderFinished={() => setProgress(0)}
-//       />
-//           <Routes>
-//             <Route path='/' element={<News setProgress={setProgress} apiKey={apiKey}  key='general' pageSize={pageSize} country='us' category='general' />}></Route>
-//             <Route path='/business' element={<News setProgress={setProgress} apiKey={apiKey} key='business' pageSize={pageSize} country='us' category='business' />}></Route>
-//             <Route path='/entertainment' element={<News setProgress={setProgress} apiKey={apiKey}  key='entertainment' pageSize={pageSize} country='us' category='entertainment' />}></Route>
-//             <Route path='/health' element={<News setProgress={setProgress} apiKey={apiKey} key='health' pageSize={pageSize} country='us' category='health' />}></Route>
-//             <Route path='/science' element={<News setProgress={setProgress} apiKey={apiKey}key='science' pageSize={pageSize} country='us' category='science' />}></Route>
-//             <Route path='/sports' element={<News setProgress={setProgress} apiKey={apiKey} key='sports' pageSize={pageSize} country='us' category='sports' />}></Route>
-//             <Route path='/technology' element={<News setProgress={setProgress} apiKey={apiKey} key='technology' pageSize={pageSize} country='us' category='technology' />}></Route>
-//           </Routes>
-//         </BrowserRouter>
-//       </div>
-//     )
-//   }
-// }
 
 
 
