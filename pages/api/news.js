5dcd0b5a773af94481098cc0d24ed238e7bb0f76
Faateh <faatehnaeem53@gmail.com>
2025-11-09 11:50:44 +0500

    added next and api

---
+++ pages/api/news.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+  const { category = 'general', country = 'us', page = 1, pageSize = 10 } = req.query;
+
+  const response = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&page=${page}&pageSize=${pageSize}&apiKey=${process.env.NEWS_API_KEY}`
+  );
+  const data = await response.json();
+  res.setHeader("Cache-Control", "no-store");
+  res.status(200).json(data);
+}
